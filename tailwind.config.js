96552465e924cdd640c4ec50a8597ec12a4107f3
“mahfuz-7148” <mrahman7148@gmail.com>
2024-08-15 21:38:56 +0600

    donee

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: 
+      {
+        title: ["Charis SIL", "serif"],
+      },
+      colors:
+            {
+              'primary': '#079211',
+              'white': '#FFFF',
+              'light-gray': '#D1D5DB',
+              'gray': '#808080',
+              'deep-gray': '#0A0A0F',
+              'light-dark': '#313131',
+              'dark': '#101017',
+              'deep-dark': '#050507',
+            },
+    },
+  },
+  plugins: [],
+}
+
